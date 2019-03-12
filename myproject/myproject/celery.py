6015080f2b3fdb8b417874b0aa80e2c8dda2b55a
Boboc Ionut-Cristian <cristi-boboc@users.noreply.github.com>
2019-03-12 14:19:45 +0200

    Update celery.py

--- myproject/myproject/celery.py
+++ myproject/myproject/celery.py
@@ -13,7 +13,7 @@ app = Celery('myproject')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
